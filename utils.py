444f3551cc88d19b4fd30a5b0a7e24c6fd224574
Scott Fear <wgetscott@gmail.com>
2026-04-10 00:38:50 +0100

    Add JSON file reader utility function to utils

--- utils.py
+++ utils.py
@@ -1,6 +1,20 @@
 import re
+import json
 
 def normalise(text: str) -> str:
+    """
+    Utility function to normalise text input.
+    """
+
     text = text.lower().replace(" ", "")
     text = re.sub(r"[^a-z0-9]", "", text) # remove puncuation and symbols
     return text
+
+
+def load_json(path: str):
+    """
+    Utility function to read JSON files.
+    """
+
+    with open(path, "r") as f:
+        return json.load(f)
