130ad572afd48f0011e11f6fae5af6c050f3b618
Scott Fear <wgetscott@gmail.com>
2026-04-10 01:57:59 +0100

    Add save_json utility function for writing JSON files

--- utils.py
+++ utils.py
@@ -2,9 +2,7 @@ import re
 import json
 
 def normalise(text: str) -> str:
-    """
-    Utility function to normalise text input.
-    """
+    """Utility function to normalise text input."""
 
     text = text.lower().replace(" ", "")
     text = re.sub(r"[^a-z0-9]", "", text) # remove puncuation and symbols
@@ -12,9 +10,14 @@ def normalise(text: str) -> str:
 
 
 def load_json(path: str):
-    """
-    Utility function to read JSON files.
-    """
+    """Utility function to read JSON files."""
 
     with open(path, "r") as f:
         return json.load(f)
+    
+
+def save_json(path: str, data):
+    """Utility function to write JSON files."""
+
+    with open(path, "w") as f:
+        json.dump(data, f, indent=2)
